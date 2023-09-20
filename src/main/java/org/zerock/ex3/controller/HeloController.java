6a38ce300a1e06471e850413290d04c05643c878
Jiwoong-Jung <jjw981119@naver.com>
2023-09-20 11:27:35 +0900

    샘플 프로젝트 1차 완성

--- src/main/java/org/zerock/ex3/controller/HeloController.java
+++ src/main/java/org/zerock/ex3/controller/HeloController.java
@@ -3,14 +3,15 @@ package org.zerock.ex3.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.zerock.ex3.entity.MyData;
 import org.zerock.ex3.repository.MyDataRepository;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HeloController {
@@ -35,6 +36,73 @@ public class HeloController {
             @ModelAttribute("formModel") MyData mydata,
             ModelAndView mav) {
         repository.saveAndFlush(mydata);
+        return new ModelAndView("redirect:/");
+    }
+
+    @PostConstruct
+    public void init(){
+        MyData d1 = new MyData();
+        d1.setName("kim");
+        d1.setAge(123);
+        d1.setMail("kim@gilbut.co.kr");
+        d1.setMemo("this is my data!");
+        repository.saveAndFlush(d1);
+        MyData d2 = new MyData();
+        d2.setName("lee");
+        d2.setAge(15);
+        d2.setMail("lee@flower");
+        d2.setMemo("my girl friend.");
+        repository.saveAndFlush(d2);
+        MyData d3 = new MyData();
+        d3.setName("choi");
+        d3.setAge(37);
+        d3.setMail("choi@happy");
+        d3.setMemo("my work friend...");
+        repository.saveAndFlush(d3);
+    }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public ModelAndView edit(@ModelAttribute MyData mydata,
+                             @PathVariable int id, ModelAndView mav) {
+        mav.setViewName("edit");
+        mav.addObject("title","edit mydata.");
+        Optional<MyData> data = repository.findById((long)id);
+        mav.addObject("formModel",data.get());
+        return mav;
+    }
+
+    @GetMapping("/edit")
+    public String edit2(@ModelAttribute MyData mydata, @RequestParam int id,
+                        Model model) {
+        Optional<MyData> data = repository.findById((long)id);
+        model.addAttribute("title", "edit mydata.");
+        model.addAttribute("formModel", data.get());
+        return "edit";
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    @Transactional(readOnly=false)
+    public ModelAndView update(@ModelAttribute MyData mydata,
+                               ModelAndView mav) {
+        repository.saveAndFlush(mydata);
+        return new ModelAndView("redirect:/");
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public ModelAndView delete(@PathVariable int id,
+                               ModelAndView mav) {
+        mav.setViewName("delete");
+        mav.addObject("title","delete mydata.");
+        Optional<MyData> data = repository.findById((long)id);
+        mav.addObject("formModel",data.get());
+        return mav;
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    @Transactional(readOnly=false)
+    public ModelAndView remove(@RequestParam long id,
+                               ModelAndView mav) {
+        repository.deleteById(id);
         return new ModelAndView("redirect:/");
     }
 
